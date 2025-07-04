84453d88f5ebaee445e6f3e7ca28586c3ff22684
Terracotich <evilrobbitop2006@gmail.com>
2025-07-04 21:52:28 +0300

    Controller for authentication page

--- skateshop/src/main/java/com/website/skateshop/controller/AuthController.java
+++ skateshop/src/main/java/com/website/skateshop/controller/AuthController.java
@@ -1,4 +1,13 @@
 package com.website.skateshop.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
