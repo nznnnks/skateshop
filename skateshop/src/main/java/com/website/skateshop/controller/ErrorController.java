2abc86f240f913d42dd683d28fbba3f857b70bf2
Terracotich <evilrobbitop2006@gmail.com>
2025-07-04 21:53:25 +0300

    Controller for treat exceptions

--- skateshop/src/main/java/com/website/skateshop/controller/ErrorController.java
+++ skateshop/src/main/java/com/website/skateshop/controller/ErrorController.java
@@ -1,4 +1,18 @@
 package com.website.skateshop.controller;
 
-public class ErrorController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @GetMapping("/access-denied")
+    public String handleAccessDenied() {
+        return "access-denied";
+    }
+
+    @GetMapping("/error")
+    public String handleError() {
+        return "error";
+    }
 }
